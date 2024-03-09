0b1778ca16fde3990637f2fd1db0db6970f7c982
Heiker Curiel <heikercuriel@gmail.com>
2024-03-09 10:57:45 -0400

    feat: add descriptions to keymaps

--- init.lua
+++ init.lua
@@ -15,8 +15,8 @@ vim.opt.termguicolors = true
 vim.g.mapleader = ' '
 
 -- Basic clipboard interaction
-vim.keymap.set({'n', 'x', 'o'}, 'gy', '"+y') -- copy
-vim.keymap.set({'n', 'x', 'o'}, 'gp', '"+p') -- paste
+vim.keymap.set({'n', 'x', 'o'}, 'gy', '"+y', {desc = 'Copy to clipboard'})
+vim.keymap.set({'n', 'x', 'o'}, 'gp', '"+p', {desc = 'Paste clipboard content'})
 
 -- ========================================================================== --
 -- ==                               PLUGINS                                == --
@@ -83,12 +83,9 @@ vim.cmd.colorscheme('tokyonight')
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 30
 
--- Toggle file explorer
 -- See :help netrw-browse-maps
-vim.keymap.set('n', '<leader>e', '<cmd>Lexplore<cr>')
-
--- Open file explorer in current folder
-vim.keymap.set('n', '<leader>E', '<cmd>Lexplore %:p:h<cr>')
+vim.keymap.set('n', '<leader>e', '<cmd>Lexplore<cr>', {desc = 'Toggle file explorer'})
+vim.keymap.set('n', '<leader>E', '<cmd>Lexplore %:p:h<cr>', {desc = 'Open file explorer in current file'})
 
 -- See :help lualine.txt
 require('lualine').setup({
@@ -120,12 +117,12 @@ require('mini.bufremove').setup({})
 vim.keymap.set('n', '<leader>bc', '<cmd>lua pcall(MiniBufremove.delete)<cr>')
 
 -- See :help telescope.builtin
-vim.keymap.set('n', '<leader>?', '<cmd>Telescope oldfiles<cr>')
-vim.keymap.set('n', '<leader><space>', '<cmd>Telescope buffers<cr>')
-vim.keymap.set('n', '<leader>ff', '<cmd>Telescope find_files<cr>')
-vim.keymap.set('n', '<leader>fg', '<cmd>Telescope live_grep<cr>')
-vim.keymap.set('n', '<leader>fd', '<cmd>Telescope diagnostics<cr>')
-vim.keymap.set('n', '<leader>fs', '<cmd>Telescope current_buffer_fuzzy_find<cr>')
+vim.keymap.set('n', '<leader>?', '<cmd>Telescope oldfiles<cr>', {desc = 'Search file history'})
+vim.keymap.set('n', '<leader><space>', '<cmd>Telescope buffers<cr>', {desc = 'Search open files'})
+vim.keymap.set('n', '<leader>ff', '<cmd>Telescope find_files<cr>', {desc = 'Search files in project'})
+vim.keymap.set('n', '<leader>fg', '<cmd>Telescope live_grep<cr>', {desc = 'Search term in project'})
+vim.keymap.set('n', '<leader>fd', '<cmd>Telescope diagnostics<cr>', {desc = 'Search diagnostics message'})
+vim.keymap.set('n', '<leader>fs', '<cmd>Telescope current_buffer_fuzzy_find<cr>', {desc = 'Search term in current file'})
 
 require('telescope').load_extension('fzf')
 
