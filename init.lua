b592e2ad2f8a278a27035630afd967e38cf40d8f
Heiker Curiel <heikercuriel@gmail.com>
2025-10-31 10:40:52 -0400

    feat: auto-install parsers on FileType event (nvim v0.11 only)

--- init.lua
+++ init.lua
@@ -196,31 +196,45 @@ require('which-key').add({
 local ts_parsers = {'lua', 'vim', 'vimdoc', 'c', 'query'}
 
 if vim.fn.has('nvim-0.11') == 1 then
-  require('nvim-treesitter').install(ts_parsers)
-
   local ts = vim.treesitter
+  local ts_installed = require('nvim-treesitter').get_installed()
+
   local ts_filetypes = vim.iter(ts_parsers)
     :map(ts.language.get_filetypes)
     :flatten()
     :fold({}, function(tbl, v)
-      tbl[v] = true
+      tbl[v] = vim.tbl_contains(ts_installed, v)
       return tbl
     end)
+
+  local ts_enable = function(buffer, lang)
+    local ok, hl = pcall(ts.query.get, lang, 'highlights')
+    if ok and hl then
+      ts.start(buffer, lang)
+    end
+  end
 
   vim.api.nvim_create_autocmd('FileType', {
     desc = 'enable treesitter',
     callback = function(event)
       local ft = event.match
-      if ts_filetypes[ft] == nil then
+      local available = ts_filetypes[ft]
+      if available == nil then
         return
       end
 
       local lang = ts.language.get_lang(ft)
-      local ok, hl = pcall(ts.query.get, lang, 'highlights')
+      local buffer = event.buf
 
-      if ok and hl then
-        ts.start(event.buf, lang)
+      if available then
+        ts_enable(buffer, lang)
+        return
       end
+
+      require('nvim-treesitter').install(lang):await(function()
+        ts_filetypes[ft] = true
+        ts_enable(buffer, lang)
+      end)
     end,
   })
 else
